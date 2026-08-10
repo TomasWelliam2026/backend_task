a6e0148758a3bffc239627e85513fa7a395cda78
Kevin <kevin.adrian9415@gmail.com>
2026-08-11 07:54:58 +0800

    complete version 0

---
+++ src/controllers/api/auth/reset.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs' ;
+import jwt from 'jsonwebtoken' ;
+import { AppDataSource } from "../../../database/datasource";
+
+import { User, UserEntity } from '../../../entities/user' ;
+import { secretOrKey } from '../../../config/config';
+
+
+export const PasswordReset = async (req:any, res:any) => {
+    const { origin, newPassword } = req.body ;
+
+    console.log(req.body) ;
+    const userRepository = AppDataSource.getRepository<User>(UserEntity) ;
+
+    // const user:any = await userRepository.findOneBy({ email: email }) ;
+
+    // if( !bcrypt.compare(origin, user.password) ) return res.json({ msg: "failure" }) ;
+
+    // user.password = await bcrypt.hash(newPassword, 10) ;
+
+    // await userRepository.save(newUser) ;
+
+    res.json({ msg: "success" }) ;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 import dotEnv from 'dotenv';
 
-dotEnv.config() ;
+import api from './routes';
 
+dotEnv.config() ;
 const app = express();
 
+app.use(bodyParser.json()) ;
+app.use(bodyParser.urlencoded()) ;
+app.use('', api) ;
+
 const PORT = process.env.PORT || 3000 ;
+
 
 app.post('/api/test', (req: Request, res: Response) => {
   return res.json({ msg: "ok" }) ;
