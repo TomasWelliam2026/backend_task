fecb64ba2f68c8e87d295cafa52b3f2dbd7f1771
TomasWelliam2026 <tomas.welliam2004@outlook.com>
2026-08-11 12:20:41 +0800

    ....

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -4,13 +4,12 @@ import { UserEntity } from "../entities/user";
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "../config/config";
 
 export const AppDataSource = new DataSource({
-  // type: "postgres",
+  type: "postgres",
   // host: DB_HOST,
   // port: Number(DB_PORT),
   // username: DB_USER,
   // password: DB_PASSWORD,
   // database: DB_NAME,
-  type: "postgres",
   host: "http://localhost",
   port: 5432,
   username: "root",
