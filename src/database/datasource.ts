9781749796e5a1fe8ca3bc719ff88baeafa1fb82
Michael Ibea <michael.ibea@bolttech.io>
2026-08-11 10:31:47 +0800

    correct AppDatasource

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -1,19 +1,17 @@
 import { DataSource } from "typeorm";
 import { UserEntity } from "../entities/user";
 
-import dotEnv from 'dotenv';
-
-dotEnv.config() ;
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "../config/config";
 
 export const AppDataSource = new DataSource({
   // type: "postgres",
-  // host: process.env.DB_HOST,
-  // port: Number(process.env.DB_PORT),
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
+  // host: DB_HOST,
+  // port: Number(DB_PORT),
+  // username: DB_USER,
+  // password: DB_PASSWORD,
+  // database: DB_NAME,
   type: "postgres",
-  host: "localhost",
+  host: "http://localhost",
   port: 5432,
   username: "root",
   password: "",
